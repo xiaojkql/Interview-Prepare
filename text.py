65cae07ecee22108a356186247aa7a7a4ebac468
xiaojkql <xiaojkql@163.com>
2019-09-19 17:04:30 +0800

    .

--- text.py
+++ text.py
@@ -1,3 +1,5 @@
-a = 'AN EFFICIENT FRAMEWORK FOR LEARNING SENTENCE REPRESENTATIONS'
-a = a.lower()
-print(a)
+count = 0
+for i in range(0, 10001):
+    if '9' in str(i):
+        count += 1
+print(count)
